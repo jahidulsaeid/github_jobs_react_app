2ea6b3a490c13954fecb0a258dcf6c3d1301351e
jahidulsaeid <jahidul.saeid@gmail.com>
2021-09-11 23:56:41 +0600

    upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
 
 function App() {
+  const { jobs, loading, error } = useFetchJobs();
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading....</h1>}
+      {error && <h1>Error. Try Fefreshing...</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
---
+++ src/useFetchJobs.js
@@ -0,0 +1,7 @@
+export default function useFetchJobs(parms, page) {
+  return {
+    jobs: [2,3,3],
+    loading: true,
+    error: true,
+  };
+}
